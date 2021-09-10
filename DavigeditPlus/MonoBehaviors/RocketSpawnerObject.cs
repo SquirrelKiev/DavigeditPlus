e9c502ad33691b24ed96544deaafb22035720bf0
SquirrelKiev <laidbackenon@outlook.com>
2021-09-10 20:09:20 +0100

    Rocket Spawner

---
+++ DavigeditPlus/MonoBehaviors/RocketSpawnerObject.cs
@@ -0,0 +1,17 @@
+﻿using Davigo.Davigedit;
+using UnityEngine;
+
+namespace DavigeditPlus
+{
+    class RocketSpawnerObject : DavigeditObject
+    {
+        public override void Process()
+        {
+            RocketSpawnerSettings rocketSpawnerSettings = gameObject.GetComponentInParent<RocketSpawnerSettings>();
+            if(rocketSpawnerSettings != null)
+            {
+                rocketSpawnerSettings.rocketSpawner = Replacement.gameObject.GetComponent<RocketSpawner>();
+            }
+        }
+    }
+}
---
+++ DavigeditPlus/MonoBehaviors/RocketSpawnerSettings.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DavigeditPlus
+{
+    class RocketSpawnerSettings : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent onPickedUp = new UnityEvent();
+        [SerializeField]
+        private UnityEvent onRespawn = new UnityEvent();
+
+
+        [SerializeField] [Min(0)]
+        private float respawnTime = 10f;
+
+        [HideInInspector]
+        public RocketSpawner rocketSpawner;
+
+
+        public void Start()
+        {
+            rocketSpawner.OnPickedUp += onPickedUp_Invoke;
+            FieldInfo field = typeof(RocketSpawner).GetField("respawnTime",BindingFlags.NonPublic | BindingFlags.Instance);
+            field.SetValue(rocketSpawner, respawnTime);
+        }
+
+        private void onPickedUp_Invoke(GameObject gameObject)
+        {
+            onPickedUp.Invoke();
+            StartCoroutine(FixedLogic.InvokeFixed(respawnTime, new Action(onRespawn.Invoke)));
+        }
+    }
+}
