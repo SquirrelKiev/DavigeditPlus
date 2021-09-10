68018e7e8a324845872eab7b1420d97408875b36
SquirrelKiev <laidbackenon@outlook.com>
2021-09-10 23:36:05 +0100

    Start of a breakable

---
+++ DavigeditPlus/MonoBehaviors/Breakable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace DavigeditPlus
+{
+    class Breakable : MonoBehaviour, IDamageable
+    {
+        [SerializeField]
+        private float health = 10f;
+
+        public void TakeDamage(float amount, DamageType damageType, Vector3 point, Vector3 direction, Player owner, float giantDamage)
+        {
+            MelonLoader.MelonLogger.Msg(amount.ToString() + damageType.ToString());
+            MelonLoader.MelonLogger.Msg(giantDamage.ToString());
+        }
+    }
+}
