b1571a0e501fad794521fe918d1cf4fe93502c02
SquirrelKiev <laidbackenon@outlook.com>
2021-09-11 15:44:26 +0100

    Breakable works

--- DavigeditPlus/MonoBehaviors/Breakable.cs
+++ DavigeditPlus/MonoBehaviors/Breakable.cs
@@ -19,33 +19,48 @@ namespace DavigeditPlus
 
         [Header("Events")]
         [SerializeField]
-        private UnityEvent OnBreak;
+        private UnityEvent onTakeDamage = new UnityEvent();
         [SerializeField]
-        private UnityEvent onTakeDamage;
+        private UnityEvent onBreak = new UnityEvent();
 
         private float currentHealth;
 
         private void Start()
         {
-            currentHealth = health;
+            ResetHealth();
         }
 
         [HideInInspector]
         public void TakeDamage(float amount, DamageType damageType, Vector3 point, Vector3 direction, Player owner, float giantDamage)
         {
+            if(CheckFilter(damageType))
+                DealDamage(giantDamage);
         }
 
         public void ResetHealth()
         {
             currentHealth = health;
         }
-        public void DealDamage(int damageAmount)
+        public void DealDamage(float damageAmount)
         {
+            onTakeDamage.Invoke();
 
+            if(health >= 0)
+            {
+                currentHealth -= damageAmount;
+                if(currentHealth < 0)
+                {
+                    Break();
+                }
+            }
         }
         public void Break()
         {
-            
+            onBreak.Invoke();
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+            }
         }
 
         private enum DamageFilter
@@ -53,6 +68,17 @@ namespace DavigeditPlus
             Concussive,
             Explosive,
             All
+        }
+
+        private bool CheckFilter(DamageType damageType)
+        {
+            if (damageFilter == DamageFilter.All)
+                return true;
+            else if (damageType == (DamageType)damageFilter)
+                return true;
+
+            // didnt pass check
+            return false;
         }
     }
 }
