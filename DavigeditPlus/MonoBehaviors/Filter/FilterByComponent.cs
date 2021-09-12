aeccd5f8754e3f64b4fb0bf4fa9797c05c0ea641
SquirrelKiev <laidbackenon@outlook.com>
2021-09-12 17:00:12 +0100

    added #7

--- DavigeditPlus/MonoBehaviors/Filter/FilterByComponent.cs
+++ DavigeditPlus/MonoBehaviors/Filter/FilterByComponent.cs
@@ -7,10 +7,60 @@ namespace DavigeditPlus.Filter
     {
         [SerializeField]
         private string componentClassName;
+        [SerializeField]
+        private bool searchParents = false, searchChildren = false;
+
+        private Type componentClass;
+
+        private void Start()
+        {
+            componentClass = Type.GetType(componentClassName);
+            if(componentClass == null)
+                componentClass = Type.GetType($"{componentClassName}, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null");
+            MelonLoader.MelonLogger.Msg(componentClass);
+        }
 
         public bool CheckFilter(GameObject filterObject)
         {
-            return true;
+            if (filterObject.GetComponent(componentClass) != null)
+                return !reverseOutcome;
+
+            bool passed = false;
+            if(searchChildren)
+                passed = SearchChildren(filterObject);
+
+            if (passed)
+                return !reverseOutcome;
+
+            if (searchParents)
+                passed = SearchParents(filterObject);
+
+            if (passed)
+                return !reverseOutcome;
+
+            return reverseOutcome;
+        }
+
+        private bool SearchChildren(GameObject gameObject)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                if (child.GetComponent(componentClass) != null)
+                    return true;
+            }
+            return false;
+        }
+
+        private bool SearchParents(GameObject gameObject)
+        {
+            Transform currentParent = gameObject.transform.parent;
+            while (currentParent != null)
+            {
+                if (currentParent.GetComponent(componentClass) != null)
+                    return true;
+                currentParent = currentParent.parent;
+            }
+            return false;
         }
     }
 }
