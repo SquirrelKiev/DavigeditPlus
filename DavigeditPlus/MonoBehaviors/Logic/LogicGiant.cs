2456c5f77f5a525e03172a86f596789d9a53d5a5
SquirrelKiev <laidbackenon@outlook.com>
2021-09-13 20:52:24 +0100

    #12 done

--- DavigeditPlus/MonoBehaviors/Logic/LogicGiant.cs
+++ DavigeditPlus/MonoBehaviors/Logic/LogicGiant.cs
@@ -1,16 +1,64 @@
 ﻿using UnityEngine;
-using DavigeditPlus.Filter;
+using System.Reflection;
+using UnityEngine.Events;
 
 namespace DavigeditPlus.Logic
 {
     class LogicGiant : Filter.Filter
     {
+        [Header("Settings")]
         [SerializeField]
         private bool leftHand = true;
         [SerializeField]
         private bool rightHand = true;
         [SerializeField]
         private bool head = true;
+
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onHeadDamaged = new UnityEvent();
+        [SerializeField]
+        private UnityEvent onDeath = new UnityEvent();
+        [SerializeField]
+        private UnityEvent onRespawn = new UnityEvent();
+
+        private GameController gameController;
+        private Giant giant;
+        private Damageable headDamageable;
+
+        private void Start()
+        {
+            gameController = SingletonBehaviour<GameController>.Instance;
+            giant = gameController.Giant;
+
+            FieldInfo giant_headDamageable = giant.GetType().GetField("headDamageable", BindingFlags.NonPublic | BindingFlags.Instance);
+            headDamageable = (Damageable)giant_headDamageable.GetValue(giant);
+
+            giant.OnTakeHeadDamage += Giant_OnTakeHeadDamage;
+            giant.OnDeath += onDeath.Invoke;
+            giant.OnRespawn += onRespawn.Invoke;
+        }
+
+        // Would have a Vector3 one too but UnityEvent doesnt like it
+        public void TeleportGiant(GameObject location)
+        {
+            FindObjectOfType<SetStartPosition>().Set(location.transform.position, location.transform.rotation);
+        }
+
+        public void RespawnGiant()
+        {
+            giant.Respawn();
+        }
+
+        public void DealDamageToGiant(float damage)
+        {
+            headDamageable.TakeDamage(damage, DamageType.Explosive, Vector3.zero, Vector3.zero, null, damage);
+        }
+
+        public void KillGiant()
+        {
+            giant.Die();
+        }
 
         public override bool CheckFilter(GameObject filterObject)
         {
@@ -23,9 +71,9 @@ namespace DavigeditPlus.Logic
             }
 
             if (
-                (hierarchy[1].name == "HandLeft" && leftHand)   ||
+                (hierarchy[1].name == "HandLeft" && leftHand) ||
                 (hierarchy[1].name == "HandRight" && rightHand) ||
-                (hierarchy[1].name == "Head" && head)           
+                (hierarchy[1].name == "Head" && head)
                )
             {
                 onPass.Invoke();
@@ -33,6 +81,11 @@ namespace DavigeditPlus.Logic
             }
 
             return false;
+        }
+
+        private void Giant_OnTakeHeadDamage(Damage obj)
+        {
+            onHeadDamaged.Invoke();
         }
     }
 }
