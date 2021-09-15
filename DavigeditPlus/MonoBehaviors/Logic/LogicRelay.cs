6f6c61cc75dbda30a256910b0a5a73c43546fa1e
SquirrelKiev <laidbackenon@outlook.com>
2021-09-15 21:37:09 +0100

    LogicRelay, #4 done

--- DavigeditPlus/MonoBehaviors/Logic/LogicRelay.cs
+++ DavigeditPlus/MonoBehaviors/Logic/LogicRelay.cs
@@ -1,9 +1,27 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace DavigeditPlus.Logic
 {
     class LogicRelay : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField]
+        private bool onlyTriggerOnce = false;
+
+        private bool triggered = true;
+
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onTrigger = new UnityEvent();
+
+        public void Trigger()
+        {
+            if (triggered)
+            {
+                if (onlyTriggerOnce) triggered = false;
+                onTrigger.Invoke();
+            }
+        }
     }
 }
