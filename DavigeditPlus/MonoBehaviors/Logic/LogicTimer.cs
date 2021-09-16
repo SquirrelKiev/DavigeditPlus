5d7a29cffa02916e9d5a0f5e4036954362bff9eb
SquirrelKiev <laidbackenon@outlook.com>
2021-09-16 09:06:05 +0100

    added #2

--- DavigeditPlus/MonoBehaviors/Logic/LogicTimer.cs
+++ DavigeditPlus/MonoBehaviors/Logic/LogicTimer.cs
@@ -1,9 +1,62 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace DavigeditPlus.Logic
 {
     class LogicTimer : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField, Tooltip("Max time before repeating, in seconds. ")]
+        private float timerInterval = 10f;
+        [SerializeField]
+        private bool autoRepeat = true;
+        
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onTimerEnded = new UnityEvent();
+
+        private Coroutine timer;
+        private float currentTimer = 0;
+        private bool paused = false;
+
+        private void Start()
+        {
+            timer = StartCoroutine(IETimer());
+        }
+
+        public void SetNewTimerInterval(float seconds)
+        {
+            timerInterval = seconds;
+        }
+
+        public void PauseTimer()
+        {
+            paused = !paused;
+        }
+
+        public void ResetTimer()
+        {
+            StopCoroutine(timer);
+            currentTimer = 0;
+            timer = StartCoroutine(IETimer());
+        }
+
+        private IEnumerator IETimer()
+        {
+            while (currentTimer <= timerInterval)
+            {
+                if(!paused)
+                    currentTimer += Time.deltaTime;
+                yield return null;
+            }
+            onTimerEnded.Invoke();
+            if (autoRepeat)
+            {
+                ResetTimer();
+            }
+            yield break;
+        }
     }
 }
