6370dfadc8c720c510cefd41d08063cf06549ecc
SquirrelKiev <laidbackenon@outlook.com>
2021-09-17 21:56:48 +0100

    added #6

--- DavigeditPlus/MonoBehaviors/Logic/LogicGame.cs
+++ DavigeditPlus/MonoBehaviors/Logic/LogicGame.cs
@@ -1,9 +1,50 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
+using System.Reflection;
 
 namespace DavigeditPlus.Logic
 {
     class LogicGame : MonoBehaviour
     {
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onScoreChanged = new UnityEvent();
+
+        private GameController gameController;
+
+        private void Start()
+        {
+            gameController = SingletonBehaviour<GameController>.Instance;
+            gameController.OnScoreChanged += (GameState state) => { onScoreChanged.Invoke(); };
+        }
+
+        public void WarriorWin()
+        {
+            PropertyInfo property = typeof(GameState).GetProperty("GameOutcome");
+            property.SetValue(gameController.State, GameState.Outcome.WarriorVictory);
+        }
+
+        public void GiantWin()
+        {
+            PropertyInfo property = typeof(GameState).GetProperty("GameOutcome");
+            property.SetValue(gameController.State, GameState.Outcome.GiantVictory);
+        }
+
+        public void Tie()
+        {
+            PropertyInfo property = typeof(GameState).GetProperty("GameOutcome");
+            property.SetValue(gameController.State, GameState.Outcome.Tie);
+        }
+
+        public void RespawnPlayers(bool invincibleForABit)
+        {
+            foreach (Player player in gameController.Players)
+            {
+                player.Respawn(gameController.PlayerSpawnPosition);
+                if (invincibleForABit)
+                    player.SetInvincibleForDuration(SingletonBehaviour<GameConstants>.Instance.PlayerInvincibilityDuration);
+            }
+        }
     }
 }
