a5a2876fd896368791825371a63257c34dd7a213
SquirrelKiev <laidbackenon@outlook.com>
2021-09-22 18:51:04 +0100

    added LogicAuto

---
+++ DavigeditPlus/MonoBehaviors/Logic/LogicAuto.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace DavigeditPlus
+{
+    class LogicAuto : MonoBehaviour
+    {
+        [Header("Events")]
+        [SerializeField]
+        private UnityEvent onEnable = new UnityEvent();
+
+        private void OnEnable()
+        {
+            onEnable.Invoke();
+        }
+    }
+}
